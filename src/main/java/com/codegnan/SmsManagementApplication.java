6382c4ee777665a61db3b16732d330dfc5f0abc8
mallikharjuna kollipati <mallikharjunakollipati42620@gmail.com>
2026-08-12 15:25:19 +0530

    Initial commit

---
+++ src/main/java/com/codegnan/SmsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.codegnan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsManagementApplication.class, args);
+	}
+
+}
